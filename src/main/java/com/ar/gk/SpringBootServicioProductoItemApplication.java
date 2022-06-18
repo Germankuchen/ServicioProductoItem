7a9a633d15a09e09357b4593e1d5b87dd78297e9
germankuchen <germankuchen@gmail.com>
2022-06-18 19:05:33 -0300

    Se agregaron métodos alternativos cuando no responde alguno.

--- src/main/java/com/ar/gk/SpringBootServicioProductoItemApplication.java
+++ src/main/java/com/ar/gk/SpringBootServicioProductoItemApplication.java
@@ -2,12 +2,14 @@ package com.ar.gk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class SpringBootServicioProductoItemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ar/gk/controllers/ProductoItemController.java
+++ src/main/java/com/ar/gk/controllers/ProductoItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ar.gk.model.ItemProducto;
+import com.ar.gk.model.Producto;
 import com.ar.gk.services.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ProductoItemController {
@@ -23,9 +25,21 @@ public class ProductoItemController {
 		return itemServices.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/listar/{id}/cantidad/{cantidad}")
 	public ItemProducto findById(@PathVariable(name = "id") Long id, @PathVariable(name = "cantidad") Integer cantidad) {
 		return itemServices.findByIdCantidad(id, cantidad);
+	}
+	
+	public ItemProducto metodoAlternativo(Long id, Integer cantidad) {
+		ItemProducto item = new ItemProducto();
+		Producto producto = new Producto();
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Llanta");
+		producto.setPrecio(1500.5);
+		item.setProducto(producto);
+		return item;
 	}
 	
 }
