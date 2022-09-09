827c6502f3eb8df54eeb899af164c2ce215f4ae8
germankuchen <germankuchen@gmail.com>
2022-09-09 19:21:49 -0300

    Se agregó resilience

--- src/main/java/com/ar/gk/SpringBootServicioProductoItemApplication.java
+++ src/main/java/com/ar/gk/SpringBootServicioProductoItemApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-@EnableCircuitBreaker
 public class SpringBootServicioProductoItemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ar/gk/controllers/ProductoItemController.java
+++ src/main/java/com/ar/gk/controllers/ProductoItemController.java
@@ -2,16 +2,20 @@ package com.ar.gk.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ar.gk.model.ItemProducto;
 import com.ar.gk.model.Producto;
 import com.ar.gk.services.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ProductoItemController {
@@ -20,18 +24,31 @@ public class ProductoItemController {
 	@Qualifier("serviceFeign")
 	private ItemService itemServices;
 	
+	@Autowired
+	private CircuitBreakerFactory cbFactory; 
+	
+	private final Logger log = LoggerFactory.getLogger(ProductoItemController.class);
+	
 	@GetMapping("/listar")
-	public List<ItemProducto> findAll() {
+	public List<ItemProducto> findAll(@RequestParam(name = "nombre", required = false) String nombre, 
+			@RequestHeader(name = "token-request", required = false) String tokenRequest) {
+		if (nombre != null) {
+			System.out.println("El request param es: " + nombre);
+		}
+		if (tokenRequest != null) {
+			System.out.println("El request header es: " + tokenRequest);
+		}
 		return itemServices.findAll();
 	}
 	
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+	//@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/listar/{id}/cantidad/{cantidad}")
 	public ItemProducto findById(@PathVariable(name = "id") Long id, @PathVariable(name = "cantidad") Integer cantidad) {
-		return itemServices.findByIdCantidad(id, cantidad);
+		return cbFactory.create("items").run(()-> itemServices.findByIdCantidad(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
 	}
 	
-	public ItemProducto metodoAlternativo(Long id, Integer cantidad) {
+	public ItemProducto metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		log.error(e.getMessage());
 		ItemProducto item = new ItemProducto();
 		Producto producto = new Producto();
 		item.setCantidad(cantidad);
